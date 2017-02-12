708ef8f676be3a2b2af5775d67477160a691ad5d
viduni94 <viduni.ushanka@gmail.com>
2017-02-12 10:00:18 +0530

    home and aboutus

--- index.php
+++ index.php
@@ -110,10 +110,10 @@
               <!-- FIRST SLIDE OVERLAY -->
               <div class="slider_overlay"></div> 
               <!-- FIRST SLIDE MAIN IMAGE -->
-              <img src="img/full-slide1.jpg" alt="img">
+              <img src="img/home/home4.jpg" alt="img">
               <!-- FIRST SLIDE CAPTION-->
               <div class="slider_caption">
-                <h2>Hi,We are Ultramodern Single pro</h2>
+                <h2>COLOURFUL IDEAS THAT CHANGE YOUR LIFE</h2>
                 <p>we are a group of experienced designers and developers</p>
                 <a href="#" class="slider_btn">Who We are</a>              
               </div>
@@ -124,10 +124,10 @@
               <!-- SECOND SLIDE OVERLAY -->
               <div class="slider_overlay"></div> 
               <!-- SECOND SLIDE MAIN IMAGE -->
-              <img src="img/full-slide1.jpg" alt="img">
+              <img src="img/home/home1.jpg" alt="img">
               <!-- SECOND SLIDE CAPTION-->
               <div class="slider_caption">
-                <h2>Hi,We are Ultramodern Single pro</h2>
+                <h2>INNOVATIVE SOLUTIONS TO HELP YOU GROW</h2>
                 <p>we are a group of experienced designers and developers</p>
                 <a href="#" class="slider_btn">Who We are</a>
               </div>
@@ -138,10 +138,10 @@
               <!-- THIRD SLIDE OVERLAY -->
               <div class="slider_overlay"></div> 
               <!-- THIRD SLIDE MAIN IMAGE -->
-              <img src="img/full-slide1.jpg" alt="img">
+              <img src="img/home/home3.jpg" alt="img">
               <!-- THIRD SLIDE CAPTION-->
               <div class="slider_caption">
-                <h2>we'll change your Idea of Design</h2>
+                <h2>TIMELESS MOMENTS THAT YOU CAN TREASURE</h2>
                 <p>we are a group of experienced designers and developers</p>
                 <a href="#" class="slider_btn">Who We are</a>                 
               </div>
@@ -177,7 +177,9 @@
               <!-- START ABOUT HEADING -->
               <div class="heading">
                 <h2 class="wow fadeInLeftBig">About Us</h2>
-                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+                <p>We are a web based creative solutions platform that looks to offer affordable and efficient solutions to a spectrum of creative requirements; Our services extend towards both retail and corporate cleints.</p>
+
+                <p>Driven by a young enthusiastic team with the passion to innovate, 22UponTwo believes in collaborating with fellow creative minds in the country to design unique approaches towards solving our clients' problems.</p>
               </div>
 
               <!-- START ABOUT CONTENT -->
@@ -191,13 +193,13 @@
                           <div class="panel-heading">
                             <h4 class="panel-title">
                               <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
-                                 <span class="fa fa-check-square-o"></span>Creative & clean Design
+                                 <span class="fa fa-check-square-o"></span>Scientific approach towards solving problems
                               </a>
                             </h4>
                           </div>
                           <div id="collapseOne" class="panel-collapse collapse in">
                             <div class="panel-body">
-                             It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). 
+                             We have a strong background in both biological and computer sciences. This ensures that every problem we encounter is approached systematically. Every step in the process of providing solutions is carefully thought out and executed with great attention to detail.
                             </div>
                           </div>
                         </div>
@@ -206,13 +208,13 @@
                           <div class="panel-heading">
                             <h4 class="panel-title">
                               <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
-                                 <span class="fa fa-check-square-o"></span>100% responsive layout
+                                 <span class="fa fa-check-square-o"></span>Simple solutions that generate effective outcomes
                               </a>
                             </h4>
                           </div>
                           <div id="collapseTwo" class="panel-collapse collapse">
                             <div class="panel-body">
-                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. 
+                            We don't believe in the existence of a single method to solve a problem. Instead, we look to provide multiple solutions that suit both the requirement and budget of our customers. We give you the luxury of choice and affordability.
                             </div>
                           </div>
                         </div>
@@ -231,21 +233,7 @@
                             </div>
                           </div>
                         </div>
-                        <!-- START SINGLE FEATURED ITEAM #4 -->
-                        <div class="panel panel-default wow fadeInLeft">
-                          <div class="panel-heading">
-                            <h4 class="panel-title">
-                              <a data-toggle="collapse" data-parent="#accordion" href="#collapseFour">
-                                <span class="fa fa-check-square-o"></span>24Hours Premium Support
-                              </a>
-                            </h4>
-                          </div>
-                          <div id="collapseFour" class="panel-collapse collapse">
-                            <div class="panel-body">
-                             It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages. 
-                            </div>
-                          </div>
-                        </div>
+                        
                       </div>
                     </div>
                   </div>
@@ -255,23 +243,23 @@
                       <div class="featured_slider">
                         <!-- SINGLE SLIDE IN THE SLIDER -->
                         <div class="single_iteam">
-                          <a href="#"> <img src="img/feature_img1.jpg" alt="img"></a>                          
+                          <a href="#"> <img src="img/aboutus/aboutus5.jpg" alt="img"></a>                          
                         </div>
                         <!-- SINGLE SLIDE IN THE SLIDER -->
                         <div class="single_iteam">
-                          <a href="#"> <img src="img/feature_img1.jpg" alt="img"></a>                          
+                          <a href="#"> <img src="img/aboutus/aboutus1.jpg" alt="img"></a>                          
                         </div>
                         <!-- SINGLE SLIDE IN THE SLIDER -->
                         <div class="single_iteam">
-                          <a href="#"> <img src="img/feature_img1.jpg" alt="img"></a>                           
+                          <a href="#"> <img src="img/aboutus/aboutus3.jpg" alt="img"></a>                           
                         </div>
                         <!-- SINGLE SLIDE IN THE SLIDER -->
                         <div class="single_iteam">
-                          <a href="#"> <img src="img/feature_img1.jpg" alt="img"></a>                           
+                          <a href="#"> <img src="img/aboutus/aboutus2.jpg" alt="img"></a>                           
                         </div>
                         <!-- SINGLE SLIDE IN THE SLIDER -->
                         <div class="single_iteam">
-                          <a href="#"> <img src="img/feature_img1.jpg" alt="img"></a>                           
+                          <a href="#"> <img src="img/aboutus/aboutus4.jpg" alt="img"></a>                           
                         </div>
                       </div>
                       <!-- END FEATURED SLIDER -->
@@ -283,42 +271,50 @@
           </div>
         </div>       
       </div>
-       <!-- BEGAIN SKILLS  -->
+       <!-- BEGIN FORMULA  -->
         <div class="row">
           <div class="col-lg-12 col-md-12">
-            <div class="skills_area">
+            <div class="skills_area" style="background-image: url(img/aboutus/formula.jpg)">
               <div class="slider_overlay"></div>
               <div class="container">               
                 <div class="skills">
                  <div class="heading">
-                    <h3 class="wow fadeInDown">Our Skills</h3>               
+                    <h3 class="wow fadeInDown">Our Formula</h3>               
                   </div>
+                  <div class="col-lg-1 col-md-1 col-sm-1"></div>
                   <!-- START SINGLE SKILL-->
-                  <div class="col-lg-3 col-md-3 col-sm-3">
+                  <div class="col-lg-2 col-md-2 col-sm-2">
                    <div class="single_skill wow fadeInUp">
                      <div id="myStat" data-dimension="150" data-text="85%" data-info="" data-width="10" data-fontsize="25" data-percent="85" data-fgcolor="#999" data-bgcolor="#fff"  data-icon="fa-task"></div>
-                      <h4>Design</h4>                      
+                      <h4>Customer Consultation</h4>                      
                     </div>
                   </div>
                   <!-- START SINGLE SKILL-->
-                  <div class="col-lg-3 col-md-3 col-sm-3">
+                  <div class="col-lg-2 col-md-2 col-sm-2">
                     <div class="single_skill wow fadeInUp">
                       <div id="myStathalf2" data-dimension="150" data-text="90%" data-info="" data-width="10" data-fontsize="25" data-percent="90" data-fgcolor="#999" data-bgcolor="#fff"  data-icon="fa-task"></div>
-                      <h4>HTML/CSS</h4>
+                      <h4>Research</h4>
                     </div>
                   </div>
                   <!-- START SINGLE SKILL-->
-                  <div class="col-lg-3 col-md-3 col-sm-3">                 
+                  <div class="col-lg-2 col-md-2 col-sm-2">                 
                     <div class="single_skill wow fadeInUp">
                       <div id="myStat2" data-dimension="150" data-text="65%" data-info="" data-width="10" data-fontsize="25" data-percent="65" data-fgcolor="#999" data-bgcolor="#fff"  data-icon="fa-task"></div>
-                      <h4>jQuery</h4>
+                      <h4>Hypothesis Testing</h4>
                     </div>
                   </div>
                   <!-- START SINGLE SKILL-->
-                  <div class="col-lg-3 col-md-3 col-sm-3">                 
+                  <div class="col-lg-2 col-md-2 col-sm-2">                 
                     <div class="single_skill wow fadeInUp">
                       <div id="myStat3" data-dimension="150" data-text="75%" data-info="" data-width="10" data-fontsize="25" data-percent="75" data-fgcolor="#999" data-bgcolor="#fff"  data-icon="fa-task"></div>
-                      <h4>WordPress</h4>
+                      <h4>Feasibility Check</h4>
+                    </div>
+                  </div>
+                  <!-- START SINGLE SKILL-->
+                  <div class="col-lg-2 col-md-2 col-sm-2">                 
+                    <div class="single_skill wow fadeInUp">
+                      <div id="myStat4" data-dimension="150" data-text="75%" data-info="" data-width="10" data-fontsize="25" data-percent="75" data-fgcolor="#999" data-bgcolor="#fff"  data-icon="fa-task"></div>
+                      <h4>Implementation</h4>
                     </div>
                   </div>
                 </div>
